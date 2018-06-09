a0c20f1e75d8d8c706f2b6cc62c945e5ca2259bd
H. Kamran <hkamran@unisontech.org>
2018-06-09 16:34:58 -0700

    Update googlelogin.py

--- googlelogin.py
+++ googlelogin.py
@@ -14,13 +14,13 @@ class GoogleSession:
         self.ses = requests.session()
         login_html = self.ses.get(url_login)
         soup_login = BeautifulSoup(login_html.content, "html.parser").find('form').find_all('input')
-        my_dict = {}
+        login_creds = {}
         for u in soup_login:
             if u.has_attr('value'):
-                my_dict[u['name']] = u['value']
+                login_creds[u['name']] = u['value']
         # override the inputs without login and pwd:
-        my_dict['Email'] = login
-        my_dict['Passwd'] = pwd
+        login_creds['Email'] = login
+        login_creds['Passwd'] = pwd
         self.ses.post(url_auth, data=my_dict)
 
     def get(self, URL):
